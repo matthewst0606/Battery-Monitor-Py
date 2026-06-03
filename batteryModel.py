80e54cdf8a56d624922b1995cb39427ad34f592b
matthewst0606 <222690777+matthewst0606@users.noreply.github.com>
2026-06-03 19:53:26 -0400

    one row of data is added for each new run

--- batteryModel.py
+++ batteryModel.py
@@ -7,6 +7,13 @@ import struct
 import datetime
 import subprocess
 
+import numpy as np
+import pandas as pd
+
+import torch
+import torch.optim as optim
+import torch.nn as nn
+
 
 
 # get battery info 
@@ -15,37 +22,42 @@ import subprocess
 # system_profiler SPPowerDataType | grep -A10 "Condition"
 
 
+
 def get_system_info():
     system_info = {}
 
-    system_info['Computer_Name'] = platform.node()
     system_info['Processor_Model'] = platform.machine()
     system_info['Version'] = platform.mac_ver()
     system_info['OS'] = platform.system() + platform.version()
-    system_info[ 'Sys_Time'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-
+    system_info[ 'Sys_Date'] = datetime.datetime.now().strftime('%d/%m/%Y')
+    system_info['Sys_Time'] = datetime.datetime.now().strftime("%H:%M:%S")
 
     for i, v in enumerate(system_info.items()):
         print(i, v)
     print("\n")
 
-get_system_info()
-
+    return system_info
 
 def get_battery_info():
+    # get output from command and split it
+    # into a list
     battery_info = subprocess.getoutput("pmset -g batt")
     batteryInfo_list = battery_info.split()
 
     battery_percent = batteryInfo_list[7].removesuffix(";")
     time_remaining = batteryInfo_list[9]
 
-    print(f"""
-        Percent Charge: {battery_percent} \n
-        Remaining Battery: {time_remaining}\n
-    """)
+    print(f"Percent Charge: {battery_percent} \n")
 
-get_battery_info()
+    if time_remaining == "(no":
+        print(f"Remaining Battery: Calculating\n")
+        while time_remaining == "(no":
+            if time_remaining !="(no)":
+                break
+    
+    print(f"Remaining Battery: {time_remaining}\n")
+    return battery_percent, time_remaining
+
 
 def get_battery_health():
     battery_health = subprocess.getoutput("system_profiler SPPowerDataType | grep -A10 \"Condition\"")
@@ -54,9 +66,49 @@ def get_battery_health():
     battery_condition = batteryHealth_list[1]
     maximum_capacity = batteryHealth_list[4]
 
-    print(f"""
-        Battery Condition: {battery_condition} \n
-        Maximum Capacity: {maximum_capacity}\n
-    """)
+    print(f"Battery Condition: {battery_condition}")
+    print(f"Maximum Capacity: {maximum_capacity}")
 
-get_battery_health()
+    return battery_condition, maximum_capacity
+
+
+system_info = get_system_info()
+battery_percent, time_remaining = get_battery_info()
+battery_condition, maximum_capacity = get_battery_health()
+
+
+
+np.random.seed(0)
+
+data_frame = pd.read_csv("data.csv")
+
+# create a new row when new data is collected
+new_row = {
+    "Date": system_info['Sys_Date'],
+    "Time": system_info['Sys_Time'],
+    "Battery_Percent": battery_percent,
+    "Time_Remaining": time_remaining,
+    "Battery_Condition": battery_condition,
+    "Maximum_Capacity": maximum_capacity
+}
+data_frame = pd.concat([data_frame, pd.DataFrame([new_row])], ignore_index=True)
+data_frame.to_csv("data.csv", index=False)
+print(data_frame)
+
+training_data = [data_frame]
+target_data = [[time_remaining]]
+
+
+N = len(data_frame)
+split_training_data = data_frame[:int(N * 0.8)]
+split_target_data = data_frame[:int(N * 0.8)]
+
+
+
+
+
+
+
+
+
+
